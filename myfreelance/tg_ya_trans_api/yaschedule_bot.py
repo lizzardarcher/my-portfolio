c19ce58bc9b00d33f6071be5799e3aa6801d0690
lizzardarcher <populator@live.com>
2024-03-01 23:13:18 +0500

    yandex-transport-tg-bot-development

--- myfreelance/tg_ya_trans_api/yaschedule_bot.py
+++ myfreelance/tg_ya_trans_api/yaschedule_bot.py
@@ -186,6 +186,7 @@ def get_schedule(station, direction, selected_date, selected_shift_type, bot, me
     action = str(station) + ' ' + str(selected_date) + ' ' + str(selected_shift_type)
     tg_user = User.objects.get(telegram_id=message.chat.id).last()
     LoggingTelegramUser.objects.create(tg_user_id=tg_user, action=action)
+    bot.send_message(message.chat.id, text=f'{tg_user} {action}')
     for sh in schedule_pool:
         print(sh)
 
